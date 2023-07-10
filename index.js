82a84a6092949220a453830b8b4af535fa5e2737
Dylan Thiermann <dylant96@gmail.com>
2023-07-10 07:42:27 -0700

    implement backspace functionality

--- index.js
+++ index.js
@@ -2,25 +2,88 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// Get the DPR and size of the canvas
 const dpr = window.devicePixelRatio;
 
-// Set the "actual" size of the canvas
 canvas.width = window.innerWidth * dpr;
 canvas.height = window.innerHeight * dpr;
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 10, 10);
+canvas.style.width = window.innerWidth + "px";
+canvas.style.height = window.innerHeight + "px";
 
 let myImageData = ctx.getImageData(0,0,canvas.width, canvas.height);
 let myData = myImageData.data;
 
-for (let i = 0; i < myData.length; i+=4) {
-    myData[i] = 0;
-    myData[i+1] = 0;
-    myData[i+2] = 0;
-    myData[i+3] = 255;
+ctx.putImageData(myImageData, 0, 0);
 
+function getColorValues(x,y) {
+    let startingIndex = 4 * (y*width + x);
+    return [myData[startingIndex],
+    myData[startingIndex + 1],
+    myData[startingIndex + 2],
+    myData[startingIndex + 3]]
 }
 
-ctx.putImageData(myImageData, 0, 0);
+ctx.font = "48px monospace";
+const measurements = ctx.measureText("a");
+const letterWidth = measurements.width;
+const heightAbove = measurements.fontBoundingBoxAscent;
+const heightBelow = measurements.fontBoundingBoxDescent;
+
+const letterHeight = heightAbove + heightBelow;
+console.log(letterWidth);
+console.log(letterHeight);
+
+let mode = "insert";
+let currentX = 0;
+let currentY = 50;
+
+ctx.fillStyle = "black";
+// ctx.fillText("Hello", 0, 50);
+
+let enterInsertModeKey = "i";
+let enterNavModeKey = "CapsLock";
+
+function handleKeydown(event) {
+    if (mode == "insert") {
+        if (event.key == enterNavModeKey) {
+            mode == "navigation";
+            return;
+        }
+        else {
+            type(event.key);
+        }
+
+    }
+    if (mode == "navigation") {
+        navigate(event.key, currentSelection);
+    }
+}
+
+function type(key) {
+    if (key == "Backspace") {
+        currentX = currentX - letterWidth;
+        ctx.fillStyle = "white";
+        ctx.fillRect(currentX, currentY - heightAbove, letterWidth, letterHeight);
+        ctx.fillStyle = "black";
+    }
+    else {
+        ctx.fillText(key, currentX, currentY);
+        currentX = currentX + letterWidth;
+    }
+
+    
+}
+
+
+
+window.addEventListener("keydown", handleKeydown);
+
+// text rendering
+
+
+// starting with an expression selected (in navigation mode)
+// options: go left/right to adjacent expressions
+// delete the current expression, stay in navigation mode
+// delete the current expression and enter a replacement
+// select parent expression
+// select first child expression
