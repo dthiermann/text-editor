b3f0ad52376588b3005c1ee427d509979ee932bb
Dylan Thiermann <dylant96@gmail.com>
2023-09-26 07:29:43 -0700

    made point class

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
+// canvas-related stuff
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const dpr = window.devicePixelRatio;
+console.log(dpr);
 
 canvas.width = window.innerWidth * dpr;
 canvas.height = window.innerHeight * dpr;
@@ -10,10 +12,6 @@ canvas.height = window.innerHeight * dpr;
 canvas.style.width = window.innerWidth + "px";
 canvas.style.height = window.innerHeight + "px";
 
-let myImageData = ctx.getImageData(0,0,canvas.width, canvas.height);
-let myData = myImageData.data;
-
-ctx.putImageData(myImageData, 0, 0);
 
 function getColorValues(x,y) {
     let startingIndex = 4 * (y*width + x);
@@ -22,6 +20,10 @@ function getColorValues(x,y) {
     myData[startingIndex + 2],
     myData[startingIndex + 3]]
 }
+
+let myImageData = ctx.getImageData(0,0,canvas.width, canvas.height);
+let myData = myImageData.data;
+ctx.putImageData(myImageData, 0, 0);
 
 ctx.font = "48px monospace";
 const measurements = ctx.measureText("a");
@@ -33,24 +35,52 @@ const letterHeight = heightAbove + heightBelow;
 console.log(letterWidth);
 console.log(letterHeight);
 
+// text editing logic:
+
+let rowWidth = 80;
+
+let rows = [[""]];
+let cursor = new Point(0,0);
+
+class Point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+    advance() {
+        this.x ++;
+    }
+    retreat() {
+        if (this.x > 0) {
+            this.x --;
+        }
+        if (this.x == 0 && this.y > 0) {
+            this.y --;
+            this.x = rows[y].length;
+        }
+    }
+}
+
 let mode = "insert";
+let backgroundColor = "white";
+let letterColor = "black";
+
 let currentX = 0;
 let currentY = 50;
 
 ctx.fillStyle = "black";
-// ctx.fillText("Hello", 0, 50);
 
 let enterInsertModeKey = "i";
 let enterNavModeKey = "CapsLock";
 
-function handleKeydown(event) {
+function handleKeydown(cursor, event) {
     if (mode == "insert") {
         if (event.key == enterNavModeKey) {
             mode == "navigation";
             return;
         }
         else {
-            type(event.key);
+            insert(cursor, event.key);
         }
 
     }
@@ -59,19 +89,57 @@ function handleKeydown(event) {
     }
 }
 
-function type(key) {
+function insert(point, key) {
     if (key == "Backspace") {
-        currentX = currentX - letterWidth;
-        ctx.fillStyle = "white";
-        ctx.fillRect(currentX, currentY - heightAbove, letterWidth, letterHeight);
-        ctx.fillStyle = "black";
+        point.retreat;
+        
     }
     else {
-        ctx.fillText(key, currentX, currentY);
-        currentX = currentX + letterWidth;
+        type(point, key);
+    }
+}
+
+function type(point, key) {
+    rows[point.x].splice(point.y, 0, key);
+}
+
+
+class Node {
+    constructor(left, right) {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+
+
+
+
+
+
+
+function retreat(place) {
+    if (place.x == 0 && place.y > 0) {
+        let y = place.y - 1;
+        let x = rowWidth * letterWidth;
+    }
+    if (place.x > 0) {
+        let x = place.x - 1;
+        let y = place.y;
+    }
+    else {
+        let x = place.x;
+        let y = place.y;
     }
 
-    
+    return new Point(x,y);
+}
+
+function clear(place) {
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(place.x, place.y - heightAbove, letterWidth, letterHeight);
+    ctx.fillStyle = letterColor;
 }
 
 
